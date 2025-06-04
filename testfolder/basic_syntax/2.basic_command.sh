6c33e3a11b835ff63a1f3b7e2abddeb7de71c88f
ggj0228 <ggj0228@naver.com>
2025-06-04 15:30:40 +0900

    branch

--- testfolder/basic_syntax/2.basic_command.sh
+++ testfolder/basic_syntax/2.basic_command.sh
@@ -56,4 +56,17 @@ git stash list
 # 저장목록 전체 삭제
 git stash clear
 
+# tag관련 명령어
+# 가장 최신의 commit에 tag명시
+git tag  태그버전명(v1.1)
+# tag release : 코드의 commit, push와 상관없이 별개로 작업진행행
+git push origin 태그버전명
+# 태그 목록 조뢰
+git tag
+
+# 브랜치 생성
+# 현재 checkout되어있는 브랜치를 기준으로 새로운 브랜치 생성
+git branch 브랜치명
+
+# 새로운 작업 시작 시에 main을 최신화한 후에 main을 기준으로 branch생성
 
